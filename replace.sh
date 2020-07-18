9569919495c71a1d790306f0f7fc69b7fa511afb
Keerthipriyau <64243542+Keerthipriyau@users.noreply.github.com>
2020-07-18 13:00:38 +0530

    Program to replace every alternative occurrence of a word "hello"

--- replace.sh
+++ replace.sh
@@ -1,28 +1,53 @@
 #replace alternative occurrences "hello" to "world"
 
 #!/bin/sh
+
 >file3.txt
+
+#set -x
+
 while read -r line
+
 do
+
    count=0
+
    wordcount=`echo $line | wc -w`
+
      for (( j=1; j<=$wordcount ; j++ ));
+
    do
+
     {
-          var="$"$j
-          word=`echo $line | awk '{print '${var}'}'`
-          if [ $word == 'hello' ]
+
+      word=`echo $line | awk -v i="$j" '{print $i}'`
+
+          if [ "$word" == "hello" ]
+
            then
+
                count=`expr $count + 1`
-             if [ `expr $count % 2` == 1 ]
+
+               modval=`expr $count % 2`
+
+               echo $modval
+
+             if [ "$modval" == "1" ]
+
              then
-             line=$(echo $line | awk ''${var}'=="hello" {'${var}'="world"}1')
-           fi
+
+                line=`echo $line | awk -v k="$j" '{$k="world"}'1`
+
+             fi
+
          fi
-   }
+
+    }
+
     done
+
     count=0
+
     echo $line >> file3.txt
 
- 
 done < file2.txt
